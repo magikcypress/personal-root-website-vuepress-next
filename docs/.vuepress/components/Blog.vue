c96ee4eba561bd3072cde3c121eb6016dad21c38
__cyp <cyp@rouquin.me>
2023-08-09 15:33:55 +0200

    Update Blog.vue

--- docs/.vuepress/components/Blog.vue
+++ docs/.vuepress/components/Blog.vue
@@ -1,5 +1,7 @@
 <template>
 
+        <blockquote><a href="/blog/Depreciate-Key-Brookworm">Depreciate Key Debian Brookworm</a> - <b>[08/09/2023]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        
         <blockquote><a href="/blog/Abreviations-M">Abreviations M.</a> - <b>[07/26/2023]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
 
         <blockquote><a href="/blog/OddCommand-1">OddCommand</a> - <b>[06/10/2023]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
