597c2168a76cd58ea56995d53e321fa3e7c589b4
magikcypress <cyp@rouquin.me>
2025-02-20 20:51:07 +0100

    Update blog entries and add new links for WhatsApp dialogue

--- docs/.vuepress/components/Blog.vue
+++ docs/.vuepress/components/Blog.vue
@@ -1,35 +1,72 @@
 <template>
 
-        <blockquote><a href="/blog/AI-Avatar-OpenAI">Update my avatar with AI (OpenAI)</a> - <b>[21/07/2024]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/whatapp">dialogue avec OpenAI sur whatapp</a> - <b>[20/02/2025]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/AI-Avatar-Cohere">Update my avatar with AI (Cohere)</a> - <b>[21/07/2024]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/AI-Avatar-OpenAI">Update my avatar with AI (OpenAI)</a> - <b>[21/07/2024]</b> -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/AI-Avatar-Otter">Update my avatar with AI (Cyber-Otter 2.0)</a> - <b>[21/07/2024]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/AI-Avatar-Cohere">Update my avatar with AI (Cohere)</a> - <b>[21/07/2024]</b> -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/Balade-Marseille">Balade à Marseille</a> - <b>[03/02/2024]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/AI-Avatar-Otter">Update my avatar with AI (Cyber-Otter 2.0)</a> - <b>[21/07/2024]</b>
+                -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/Cookies">Cookies</a> - <b>[02/15/2024]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Balade-Marseille">Balade à Marseille</a> - <b>[03/02/2024]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/Bill-Internet-Provider">Bill On My Internet Provider</a> - <b>[09/06/2023]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Cookies">Cookies</a> - <b>[02/15/2024]</b> -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/Depreciate-Key-Brookworm">Depreciate Key Debian Brookworm</a> - <b>[08/09/2023]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
-        
-        <blockquote><a href="/blog/Abreviations-M">Abreviations M.</a> - <b>[07/26/2023]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Bill-Internet-Provider">Bill On My Internet Provider</a> - <b>[09/06/2023]</b> -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/OddCommand-1">OddCommand</a> - <b>[06/10/2023]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Depreciate-Key-Brookworm">Depreciate Key Debian Brookworm</a> - <b>[08/09/2023]</b> -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/gaufrette">Gaufrette</a> - <b>[05/25/2023]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
-        
-        <blockquote><a href="/blog/Crush">Crush</a> - <b>[05/16/2023]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Abreviations-M">Abreviations M.</a> - <b>[07/26/2023]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/Anniv">Anniv</a> - <b>[05/16/2023]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/OddCommand-1">OddCommand</a> - <b>[06/10/2023]</b> -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/Eze">Eze</a> - <b>[05/16/2023]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/gaufrette">Gaufrette</a> - <b>[05/25/2023]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/OddCommand">OddCommand</a> - <b>[03/09/2023]</b> - <Badge type="tip" text="en" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Crush">Crush</a> - <b>[05/16/2023]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/disparition">Disparition</a> - <b>[10/16/2022]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Anniv">Anniv</a> - <b>[05/16/2023]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
 
-        <blockquote><a href="/blog/ours">L'ours</a> - <b>[10/16/2022]</b> - <Badge type="tip" text="fr" vertical="middle" /></blockquote>
+        <blockquote><a href="/blog/Eze">Eze</a> - <b>[05/16/2023]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
+
+        <blockquote><a href="/blog/OddCommand">OddCommand</a> - <b>[03/09/2023]</b> -
+                <Badge type="tip" text="en" vertical="middle" />
+        </blockquote>
+
+        <blockquote><a href="/blog/disparition">Disparition</a> - <b>[10/16/2022]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
+
+        <blockquote><a href="/blog/ours">L'ours</a> - <b>[10/16/2022]</b> -
+                <Badge type="tip" text="fr" vertical="middle" />
+        </blockquote>
 
 </template>
